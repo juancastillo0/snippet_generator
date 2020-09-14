a29e4742e9235d8401dec89d645578bba4aa66af
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2020-09-13 23:47:08 -0500

    when for sum types

--- lib/templates.dart
+++ lib/templates.dart
@@ -1,60 +1,37 @@
 import 'package:snippet_generator/models.dart';
 
-extension TemplateTypeConfig on TypeConfig {
-  String templateSumType() {
-    return """
-class $name {
-  
-  ${classes.value.map((c) => "factory $name.${c.name}(${c.templateFactoryParams()}) = _${c.name}._;").join("\n  ")}
-  
-  ${isSerializable ? templateSymTypeFromJson() : ""}
-  }
-  
-  ${classes.value.map((c) => c.templateClass()).join("\n")}
-  """;
-  }
-
-  String templateSymTypeFromJson() {
-    return """
-static $name fromJson(Map<String, dynamic> map) {
-  switch (map["type"]) {
-    ${classes.value.map((e) => "case '${e.name}': return _${e.name}.fromJson(map);").join("\n    ")}
-    default:
-      return null;
-  }
+extension CasingString on String {
+  String firstToLowerCase() => substring(0, 1).toLowerCase() + substring(1);
 }
-""";
-  }
-}
-
-String _required(bool isRequired) => isRequired ? "@required " : "";
 
 extension TemplateClassConfig on ClassConfig {
   String get className => typeConfig.isSumType ? "_$name" : typeConfig.name;
+  String get _classConstructor =>
+      '$className${typeConfig.isSumType ? "._" : ""}';
 
   String templateClass() {
     return """
-class $className {
+class $className ${typeConfig.isSumType ? "extends ${typeConfig.name}" : ""}{
   ${properties.value.map((p) => 'final ${p.type.text} ${p.name.text};').join('\n  ')}
 
-  const $className${typeConfig.isSumType ? "._" : ""}(${templateClassParams()});
+  const $_classConstructor(${_templateClassParams()})${typeConfig.isSumType ? ": super._()" : ""};
 
-  ${typeConfig.isSerializable ? templateClassFromJson() : ""}
+  ${typeConfig.isSerializable ? _templateClassFromJson() : ""}
 }
   """;
   }
 
-  String templateClassFromJson() {
+  String _templateClassFromJson() {
     return """
 static $className fromJson(Map<String, dynamic> map) {
-    return $className(
-      ${properties.value.map((e) => "${e.name.text}: map['${e.name.text}'],").join("\n      ")}
+    return $_classConstructor(
+      ${properties.value.map((e) => "${e.name.text}: map['${e.name.text}'] as ${e.type.text},").join("\n      ")}
     );
   }
 """;
   }
 
-  String templateClassParams() {
+  String _templateClassParams() {
     return """{
     ${properties.value.map((p) => '${_required(p.isRequired.value)}this.${p.name.text},').join('\n    ')}
   }""";
@@ -66,3 +43,38 @@ static $className fromJson(Map<String, dynamic> map) {
   }""";
   }
 }
+
+extension TemplateTypeConfig on TypeConfig {
+  String templateSumType() {
+    return """
+abstract class $name {
+  const $name._();
+  
+  ${classes.value.map((c) => "factory $name.${c.name.firstToLowerCase()}(${c.templateFactoryParams()}) = _${c.name}._;").join("\n  ")}
+  
+  T when<T>({${classes.value.map((c) => "@required T Function(${c.className} value) ${c.name.firstToLowerCase()},").join("\n    ")}}){
+    final v = this;
+    ${classes.value.map((c) => "if (v is ${c.className}) return ${c.name.firstToLowerCase()}(v);").join("\n    ")}
+    throw "";
+  }
+  ${isSerializable ? _templateSymTypeFromJson() : ""}
+  }
+  
+  ${classes.value.map((c) => c.templateClass()).join("\n")}
+  """;
+  }
+
+  String _templateSymTypeFromJson() {
+    return """
+static $name fromJson(Map<String, dynamic> map) {
+  switch (map["runtimeType"] as String) {
+    ${classes.value.map((e) => "case '${e.name}': return _${e.name}.fromJson(map);").join("\n    ")}
+    default:
+      return null;
+  }
+}
+""";
+  }
+}
+
+String _required(bool isRequired) => isRequired ? "@required " : "";
