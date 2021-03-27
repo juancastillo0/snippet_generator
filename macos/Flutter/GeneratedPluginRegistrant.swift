b151d4a3c961908d758023443f4f86b998e1007f
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-03-27 15:03:45 -0500

    file selector macos

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,15 +2,15 @@
 //  Generated file. Do not edit.
 //
 
-// clang-format off
-
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import path_provider_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
