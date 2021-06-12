ab7409f720a640f20133f60a0364c194409b2f99
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-06-12 21:00:26 +0000

    responsive appbar and import export modal

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -4,10 +4,12 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:snippet_generator/notifiers/rebuilder.dart';
 import 'package:snippet_generator/types/root_store.dart';
 import 'package:snippet_generator/utils/download_json.dart';
 import 'package:snippet_generator/utils/extensions.dart';
+import 'package:snippet_generator/widgets/portal/custom_overlay.dart';
+import 'package:snippet_generator/widgets/portal/global_stack.dart';
+import 'package:snippet_generator/widgets/portal/portal_utils.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 ButtonStyle _actionButton(BuildContext context) {
@@ -31,7 +33,7 @@ ButtonStyle _actionButton(BuildContext context) {
 ButtonStyle _actionButtonPadded(BuildContext context) =>
     _actionButton(context).copyWith(
       padding: MaterialStateProperty.all(
-        const EdgeInsets.symmetric(horizontal: 17),
+        const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
       ),
     );
 
@@ -91,50 +93,38 @@ class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     final rootStore = RootStore.of(context);
-    // print(Platform.resolvedExecutable);
 
-    return AppBar(
-      title: Rebuilder(builder: (context) {
-        return SizedBox(
-          height: 46,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              Center(child: Text('Flutter Snippet Generator')),
-              SizedBox(width: 30),
-              TabButton(tab: AppTabs.types),
-              TabButton(tab: AppTabs.ui),
-              TabButton(tab: AppTabs.theme),
-              TabButton(tab: AppTabs.database),
-            ],
-          ),
-        );
-      }),
-      actions: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text("Dark Mode"),
-            rootStore.themeModeNotifier.rebuild(
-              (mode) => Switch(
-                value: rootStore.themeModeNotifier.value == ThemeMode.dark,
-                onChanged: (value) {
-                  rootStore.themeModeNotifier.value =
-                      value ? ThemeMode.dark : ThemeMode.light;
+    return LayoutBuilder(builder: (context, box) {
+      final showButtons = box.maxWidth > 1200;
+      final topButtonStyle = _actionButtonPadded(context);
+      final buttonStyle = showButtons
+          ? topButtonStyle
+          : _actionButtonPadded(context).copyWith(
+              foregroundColor: MaterialStateProperty.resolveWith(
+                (states) {
+                  final base = Theme.of(context).colorScheme.onSurface;
+                  return base;
                 },
               ),
-            ),
-          ],
-        ),
-        const SizedBox(width: 20),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) {
+                  final base = Theme.of(context).colorScheme.primary;
+                  return states.contains(MaterialState.hovered)
+                      ? base.withOpacity(0.2)
+                      : null;
+                },
+              ),
+            );
+
+      final persistenceActions = [
         TextButton.icon(
-          style: _actionButtonPadded(context),
+          style: topButtonStyle,
           onPressed: rootStore.saveHive,
           icon: const Icon(Icons.save),
           label: const Text("Save"),
         ),
         TextButton.icon(
-          style: _actionButtonPadded(context),
+          style: buttonStyle,
           onPressed: () async {
             final jsonString = await importFromClient();
             if (jsonString != null) {
@@ -145,26 +135,93 @@ class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
           label: const Text("Import"),
         ),
         TextButton.icon(
-          style: _actionButtonPadded(context),
+          style: buttonStyle,
           onPressed: () {
             rootStore.downloadJson();
           },
           icon: const Icon(Icons.file_download),
           label: const Text("Export"),
         ),
-        IconButton(
-          onPressed: () {
-            launch('https://github.com/juancastillo0/snippet_generator');
-          },
-          icon: Image.asset(
-            'assets/images/GitHub-Mark-64px.png',
-            height: 32,
-            color: Colors.white,
+      ];
+
+      return AppBar(
+        title: SizedBox(
+          height: 46,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              if (box.maxWidth > 1000)
+                const Center(
+                  child: Text('Flutter Snippet Generator'),
+                ),
+              if (box.maxWidth > 1000) const SizedBox(width: 30),
+              const TabButton(tab: AppTabs.types),
+              const TabButton(tab: AppTabs.ui),
+              const TabButton(tab: AppTabs.theme),
+              const TabButton(tab: AppTabs.database),
+            ],
           ),
-          tooltip: 'Github Repository',
-        )
-      ],
-    );
+        ),
+        actions: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("Dark Mode"),
+              rootStore.themeModeNotifier.rebuild(
+                (mode) => Switch(
+                  value: rootStore.themeModeNotifier.value == ThemeMode.dark,
+                  onChanged: (value) {
+                    rootStore.themeModeNotifier.value =
+                        value ? ThemeMode.dark : ThemeMode.light;
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 20),
+          if (showButtons)
+            ...persistenceActions
+          else
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                persistenceActions.first,
+                CustomOverlayButton(
+                  builder: StackPortal.make,
+                  params: PortalParams(
+                    childAnchor: Alignment.bottomRight,
+                    portalAnchor: Alignment.topRight,
+                    portalWrapper:
+                        makeDefaultPortalWrapper(padding: EdgeInsets.zero),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  portalBuilder: (p) => Column(
+                    children: [...persistenceActions.skip(1)],
+                  ),
+                  child: Icon(
+                    Icons.arrow_drop_down_outlined,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                )
+              ],
+            ),
+          IconButton(
+            splashRadius: 24,
+            onPressed: () {
+              launch('https://github.com/juancastillo0/snippet_generator');
+            },
+            icon: Image.asset(
+              'assets/images/GitHub-Mark-64px.png',
+              height: 32,
+              color: Colors.white,
+            ),
+            tooltip: 'Github Repository',
+          ),
+          const SizedBox(width: 10),
+        ],
+      );
+    });
   }
 
   @override
