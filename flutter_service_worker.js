c7b95036e9a05e7d25abe303165527f18a245107
juancastillo0 <juancastillo0@users.noreply.github.com>
2021-07-10 18:36:16 +0000

    deploy: dae5c62843bb6bc8ff37768c148a48d2c158d0a6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,52 +3,52 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"version.json": "479ca84fb350de76102ed1d9911e3d0a",
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "427a64cee6856ec98a59b5311d446d10",
+"canvaskit_0.28.1/full/canvaskit.wasm": "bc173722c9a25e0d9217e419e454bf96",
+"canvaskit_0.28.1/full/canvaskit.js": "a14485d0b10aab99a2c105b164f8d0f0",
+"canvaskit_0.28.1/canvaskit.wasm": "94da52ff225af2ee7b498ca694692a1f",
+"canvaskit_0.28.1/canvaskit.js": "310f62861c06dbb592838bee033072eb",
+"canvaskit_0.28.1/profiling/canvaskit.wasm": "3d45442c520e5c8f13dcacd24c64ce0f",
+"canvaskit_0.28.1/profiling/canvaskit.js": "7e671cf059b389ce2e723ba2106bd0f3",
+"index.html": "f4262813e7b0c3ff4ff19f2d53ca6e63",
+"/": "f4262813e7b0c3ff4ff19f2d53ca6e63",
 "manifest.json": "16a28f96f9b8bcebc47bd6476d94c2ee",
-"assets/AssetManifest.json": "afa4391c2be4c0ec3cf6e9f2ddf15747",
-"assets/google_fonts/NunitoSans-SemiBold.ttf": "bd318b58018198a57723f311627492ac",
-"assets/google_fonts/NunitoSans-Italic.ttf": "2d517b40dabe232416b73e3a721dc950",
-"assets/google_fonts/NunitoSans-ExtraLight.ttf": "6aea75496b0ccb484d81a97920d2e64c",
-"assets/google_fonts/NunitoSans-BoldItalic.ttf": "655ce9395fcf8c21f45cfeca5bb280a4",
-"assets/google_fonts/NunitoSans-ExtraBold.ttf": "505a059580cfbeaccdcb7a489bb67ec9",
-"assets/google_fonts/NunitoSans-Regular.ttf": "4c8f447011eef80831b45edb1e5971e0",
-"assets/google_fonts/Cousine-Regular.ttf": "64a889644e439ac4806c8e41bc9d1c83",
-"assets/google_fonts/Cousine-BoldItalic.ttf": "1038b5579146b28e9e4dc98c8fc5d1d9",
-"assets/google_fonts/NunitoSans-Light.ttf": "74d36921be67fb8482bfd7324bd86790",
-"assets/google_fonts/NunitoSans-Black.ttf": "d95152ab5a160491d28b3fce25bf4ff2",
-"assets/google_fonts/OFL.txt": "4e3d9becbf87c0341298fadf87ae4d36",
-"assets/google_fonts/Cousine-Bold.ttf": "06dae6a1a3247bd76125cfe3b3480557",
-"assets/google_fonts/NunitoSans-BlackItalic.ttf": "75ec9078a3f7472f3cdee1d312a390a2",
-"assets/google_fonts/NunitoSans-ExtraLightItalic.ttf": "cf8d9c6c81866d3bdfc1f08d6ea80d8d",
-"assets/google_fonts/Cousine-Italic.ttf": "177a3d2157da07498e805683e8cdaa8d",
-"assets/google_fonts/NunitoSans-SemiBoldItalic.ttf": "b16342e303cde3bafe2d8746be885ca2",
-"assets/google_fonts/NunitoSans-ExtraBoldItalic.ttf": "2ae455ab84d04fec2d436151e712848f",
-"assets/google_fonts/LICENSE.txt": "3b83ef96387f14655fc854ddc3c6bd57",
-"assets/google_fonts/NunitoSans-LightItalic.ttf": "d395ff0f45e6b030608de646ec278a35",
-"assets/google_fonts/NunitoSans-Bold.ttf": "08e53a516d2ba719d98da46c49b3c369",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/assets/images/GitHub-Mark-64px.png": "438c17272c5f0e9f4a6da34d3e4bc5bd",
 "assets/NOTICES": "8191bac7970ef63b8a9f5c4d5dfa716a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "0f390aeba98946527b4ed9a11ae7564d",
-"canvaskit_0.25.0/canvaskit.js": "95226282bb562bd618d4df577b10abc6",
-"canvaskit_0.25.0/full/canvaskit.js": "d6f5d25a9443bb9507341afc8fd058c5",
-"canvaskit_0.25.0/full/canvaskit.wasm": "4238f9424ec50c2b1c6b184b542cada7",
-"canvaskit_0.25.0/canvaskit.wasm": "d6a15b04c709e58b6a21f32153173d4d",
-"canvaskit_0.25.0/profiling/canvaskit.js": "33f420892f483aec2826bab7d8a339ea",
-"canvaskit_0.25.0/profiling/canvaskit.wasm": "8af2a2a16416d9456e2904ac8e1e17f4",
-"canvaskit_0.27.0/canvaskit.js": "5a041372c2d254ef7b0d6545c87e7fc6",
-"canvaskit_0.27.0/full/canvaskit.js": "6e6d7a4be45a3ce9474c851c1361c566",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/images/GitHub-Mark-64px.png": "438c17272c5f0e9f4a6da34d3e4bc5bd",
+"assets/AssetManifest.json": "afa4391c2be4c0ec3cf6e9f2ddf15747",
+"assets/google_fonts/NunitoSans-Italic.ttf": "2d517b40dabe232416b73e3a721dc950",
+"assets/google_fonts/NunitoSans-Regular.ttf": "4c8f447011eef80831b45edb1e5971e0",
+"assets/google_fonts/NunitoSans-LightItalic.ttf": "d395ff0f45e6b030608de646ec278a35",
+"assets/google_fonts/NunitoSans-BlackItalic.ttf": "75ec9078a3f7472f3cdee1d312a390a2",
+"assets/google_fonts/NunitoSans-Light.ttf": "74d36921be67fb8482bfd7324bd86790",
+"assets/google_fonts/NunitoSans-ExtraLightItalic.ttf": "cf8d9c6c81866d3bdfc1f08d6ea80d8d",
+"assets/google_fonts/NunitoSans-ExtraLight.ttf": "6aea75496b0ccb484d81a97920d2e64c",
+"assets/google_fonts/NunitoSans-ExtraBold.ttf": "505a059580cfbeaccdcb7a489bb67ec9",
+"assets/google_fonts/Cousine-BoldItalic.ttf": "1038b5579146b28e9e4dc98c8fc5d1d9",
+"assets/google_fonts/NunitoSans-SemiBold.ttf": "bd318b58018198a57723f311627492ac",
+"assets/google_fonts/NunitoSans-Bold.ttf": "08e53a516d2ba719d98da46c49b3c369",
+"assets/google_fonts/NunitoSans-SemiBoldItalic.ttf": "b16342e303cde3bafe2d8746be885ca2",
+"assets/google_fonts/NunitoSans-ExtraBoldItalic.ttf": "2ae455ab84d04fec2d436151e712848f",
+"assets/google_fonts/Cousine-Italic.ttf": "177a3d2157da07498e805683e8cdaa8d",
+"assets/google_fonts/Cousine-Regular.ttf": "64a889644e439ac4806c8e41bc9d1c83",
+"assets/google_fonts/Cousine-Bold.ttf": "06dae6a1a3247bd76125cfe3b3480557",
+"assets/google_fonts/NunitoSans-Black.ttf": "d95152ab5a160491d28b3fce25bf4ff2",
+"assets/google_fonts/LICENSE.txt": "3b83ef96387f14655fc854ddc3c6bd57",
+"assets/google_fonts/NunitoSans-BoldItalic.ttf": "655ce9395fcf8c21f45cfeca5bb280a4",
+"assets/google_fonts/OFL.txt": "4e3d9becbf87c0341298fadf87ae4d36",
+"version.json": "479ca84fb350de76102ed1d9911e3d0a",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "canvaskit_0.27.0/full/canvaskit.wasm": "839bccd1dc1e0376519953a273d84559",
+"canvaskit_0.27.0/full/canvaskit.js": "6e6d7a4be45a3ce9474c851c1361c566",
 "canvaskit_0.27.0/canvaskit.wasm": "a4a607e2d11af93cba9b1cba37cad5bd",
-"canvaskit_0.27.0/profiling/canvaskit.js": "e688a5e10b47ff2872abb8b530cb79f5",
+"canvaskit_0.27.0/canvaskit.js": "5a041372c2d254ef7b0d6545c87e7fc6",
 "canvaskit_0.27.0/profiling/canvaskit.wasm": "fe6eecbad0854ed2cfadd652e9709484",
-"index.html": "9330481518eebcd41f1a53a5730826f5",
-"/": "9330481518eebcd41f1a53a5730826f5"
+"canvaskit_0.27.0/profiling/canvaskit.js": "e688a5e10b47ff2872abb8b530cb79f5"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -95,7 +95,7 @@ self.addEventListener("activate", function(event) {
         return;
       }
       var oldManifest = await manifest.json();
-      var origin = (self.location.origin + '/snippet_generator');
+      var origin = (self.location.origin + "/snippet_generator");
       for (var request of await contentCache.keys()) {
         var key = request.url.substring(origin.length + 1);
         if (key == "") {
@@ -134,7 +134,7 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== 'GET') {
     return;
   }
-  var origin = (self.location.origin + '/snippet_generator');
+  var origin = (self.location.origin + "/snippet_generator");
   var key = event.request.url.substring(origin.length + 1);
   // Redirect URLs to the index.html
   if (key.indexOf('?v=') != -1) {
